6ccc19a90ae5b9f0717cf289bf90aa8486302029
Matheus Gomes <79264192+matheusngs@users.noreply.github.com>
2023-07-13 16:17:21 -0400

    Enviando dados digitados para outra activity

--- app/src/main/java/br/com/fabricasinapse/fabricasinapse_live_android_introducao/MainActivity.kt
+++ app/src/main/java/br/com/fabricasinapse/fabricasinapse_live_android_introducao/MainActivity.kt
@@ -1,12 +1,14 @@
 package br.com.fabricasinapse.fabricasinapse_live_android_introducao
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
         val etNome = findViewById<EditText>(R.id.etNome)
         val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val btEnviar2 = findViewById<Button>(R.id.btEnviar2)
 
         btEnviar.setOnClickListener {
             if (etNome.text.isNotBlank()) {
@@ -21,6 +24,21 @@ class MainActivity : AppCompatActivity() {
             } else {
                 etNome.error = getString(R.string.type_your_name)
             }
+
+            btEnviar2.setOnClickListener {
+                if (etNome.text.isNotBlank()) {
+                    val nomeDigitado = etNome.text.toString()
+
+                    val intent = Intent(this, ResultActivity::class.java)
+
+                    intent.putExtra("NOME_DIGITADO", nomeDigitado)
+
+                    startActivity(intent)
+                } else {
+                    etNome.error = getString(R.string.type_your_name)
+                }
+            }
+
         }
 
     }
